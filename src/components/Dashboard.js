3c101bc1a85bb339938fe1a823393b09461cade5
Natalia Martian <natalia.martian@gmail.com>
2020-12-17 16:36:35 -0500

    Implement panels view based on panel id - switch between single panel and all panels view

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import classnames from "classnames";
 
 import Loading from './Loading';
+import Panel from './Panel';
 
 const data = [
   {
@@ -29,18 +30,61 @@ const data = [
 
 class Dashboard extends Component {
 
+  //default state
   state = {
-    loading: false
+    loading: false,
+    focused: null
+  }
+
+  //changes state based on value of focused
+  //if previous is not null, show null
+  //otherwise show panel with specific id
+  selectPanel = id => {
+
+    this.setState(previousState => ({
+      focused: previousState.focused !== null? null : id
+    }));
+    
   }
 
   render() {
-    const dashboardClasses = classnames("dashboard");
 
+    //renders classes conditionally based on focused state
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+     });
+
+    //if loading is true, show "Loading" component
     if (this.state.loading) {
       return <Loading />
     }
 
-    return <main className={dashboardClasses} />;
+    //filters panels depending on the focused state
+    //If this.state.focused is null then return true for every panel
+    //If this.state.focused is equal to the Panel, then let it through the filter
+    //next we map through the data to render the component
+    const panels = data
+      .filter(
+        panel => this.state.focused === null || this.state.focused === panel.id
+      )
+      .map(panel => 
+        (
+          <Panel
+            key={panel.id}
+            id={panel.id}
+            label={panel.label}
+            value={panel.value}
+            onSelect={this.selectPanel}
+          />
+        )
+      )
+
+    return (
+      <main className={dashboardClasses}>
+        {panels}
+      </main>
+
+    );
   }
 }
 
